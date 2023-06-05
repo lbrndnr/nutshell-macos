312b13f0ca3f2ff4012f0c4820ae27ab34bbea12
Laurin Brandner <mail@laurinbrandner.ch>
2023-06-05 10:32:05 +0200

    Receive transcript on main RunLoop

--- Nutshell/ContentView.swift
+++ Nutshell/ContentView.swift
@@ -11,15 +11,20 @@ import Combine
 
 struct ContentView: View {
     
-    @StateObject var transcriber = Transcriber()
+    var transcriber = Transcriber()
     @State var recording = false
+    @State var transcript = ""
     @State private var searchQuery = ""
+    
+    @State private var subscriptions = Set<AnyCancellable>()
     
     var body: some View {
         ScrollView {
-            Text(verbatim: transcriber.transcript)
+            Text(verbatim: transcript)
+                .font(.system(.body, design: .rounded))
                 .frame(maxWidth: .infinity, alignment: .leading)
         }
+        .padding()
         .toolbar {
             ToolbarItemGroup {
                 TextField("􀊫 Search", text: $searchQuery)
@@ -44,6 +49,10 @@ struct ContentView: View {
         .navigationTitle("Nutshell")
         .onAppear {
             self.transcriber.updateAvailableContent()
+            transcriber.$transcript
+                .receive(on: RunLoop.main)
+                .assign(to: \.transcript, on: self)
+                .store(in: &subscriptions)
         }
     }
     
@@ -51,6 +60,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(transcript: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vulputate semper lorem eget aliquam. Vivamus nulla turpis, interdum quis eleifend sed, sagittis sed felis. In ac velit pretium, facilisis erat quis, ullamcorper nulla. Vivamus non est sem. Nulla placerat, tortor vel tincidunt pretium, eros erat posuere risus, hendrerit commodo dui tortor eu ante. Nulla facilisi. Maecenas id neque ac dui rhoncus sollicitudin. Vestibulum mi leo, commodo eu posuere sed, consectetur sit amet massa. Suspendisse sit amet tempor quam. Vestibulum scelerisque massa massa, vel congue ex aliquet quis. Suspendisse fermentum erat id sem ornare pharetra. Aliquam sit amet nisl eget dui interdum tincidunt.")
     }
 }
